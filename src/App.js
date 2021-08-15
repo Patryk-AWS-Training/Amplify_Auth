b492d0b7343aaac2d519bd2dd7f16d50b8561742
Patryk <patryk.szylin.dev@gmail.com>
2021-08-15 18:55:38 +0100

    wrapped app in safe area provider

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Provider} from 'react-redux';
 import {NavigationContainer} from '@react-navigation/native';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {Provider as UIProvider} from 'react-native-paper';
 
 import store from './redux';
@@ -9,11 +10,13 @@ import StackRoot from './navigation';
 const App = () => {
   return (
     <Provider store={store}>
-      <UIProvider>
-        <NavigationContainer>
-          <StackRoot />
-        </NavigationContainer>
-      </UIProvider>
+      <SafeAreaProvider>
+        <UIProvider>
+          <NavigationContainer>
+            <StackRoot />
+          </NavigationContainer>
+        </UIProvider>
+      </SafeAreaProvider>
     </Provider>
   );
 };
